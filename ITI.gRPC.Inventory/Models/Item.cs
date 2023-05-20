a722099a80bfb9ca8f873cb54e9f99fb88d95434
Elshaymaa Ahmed <elshaymaaahmed@gmail.com>
2023-05-20 16:52:34 +0300

    gRPC Inventory Service

---
+++ ITI.gRPC.Inventory/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace ITI.gRPC.Inventory.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ ITI.gRPC.Inventory/Services/OrderInventoryService.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using ITI.gRPC.Inventory.Models;
+using ITI.gRPC.Inventory.Protos;
+using static ITI.gRPC.Inventory.Protos.InventoryService;
+
+namespace ITI.gRPC.Inventory.Services
+{
+    public class OrderInventoryService : InventoryServiceBase
+    {
+        private static List<Item> _items;
+        public OrderInventoryService()
+        {
+            _items = new List<Item>()
+            {
+                new Item {Id = 1, Quantity = 10, Price = 500 },
+                new Item {Id = 2, Quantity = 20, Price = 1000 },
+                new Item {Id = 3, Quantity = 30, Price = 1500 },
+            };
+        }
+
+        public override Task<InventoryResponse> AddNewItem(InventoryMessage request, ServerCallContext context)
+        {
+            bool success = false;
+            string message = "";
+
+            Item? item = _items.FirstOrDefault(i => i.Id == request.ItemID);
+
+            if (item != null)
+            {
+                if (item.Quantity >= request.Quantity) 
+                {
+                    item.Quantity -= request.Quantity;
+
+                    success = true;
+                    message = "Item has been added successfully!";
+                }
+                else
+                {
+                    message = "Sorry! There is no enough quantity in the inventory.";
+                }
+            }
+            else
+            {
+                message = "Can not find this item!";
+            }
+
+            var response = new InventoryResponse() { Success = success, Message = message};
+
+            return Task.FromResult(response);
+        }
+    }
+}
